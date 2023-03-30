6e2b36516a79071d0ddb011fbefe489dc378cb64
Suratzaza <120245703+Suratzaza@users.noreply.github.com>
2023-03-30 15:18:01 +0700

    Create Viperxz.lua

---
+++ Viperxz.lua
@@ -0,0 +1,1 @@
+if not syn then getgenv().syn={queue_on_teleport=queue_on_teleport or function()end}end loadstring(game:HttpGet("https://raw.githubusercontent.com/onehalla/1hallascripts/master/release/viperhub.lua", true))()
